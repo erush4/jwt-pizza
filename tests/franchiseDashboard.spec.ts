1ca18cd49d65d8fc822aa83cae105bdc7e78b8e6
erush4 <=>
2026-02-10 23:58:53 -0700

    admin dashboard test

--- tests/franchiseDashboard.spec.ts
+++ tests/franchiseDashboard.spec.ts
@@ -1,5 +1,11 @@
 import { expect, test } from "playwright-test-coverage";
-import { authMock, getFranchisesMock, login, validUsers, getUserFranchiseMock} from "./mocks";
+import {
+  authMock,
+  getFranchisesMock,
+  login,
+  validUsers,
+  getUserFranchiseMock,
+} from "./mocks";
 
 test.beforeEach(async ({ page }) => {
   await authMock(page);
@@ -24,18 +30,17 @@ test("franchise dashboard opens", async ({ page }) => {
   expect(storeCount).toEqual(3);
 });
 
-// test("admin dashboard opens", async ({ page }) => {
-//   const user = validUsers["admin"];
-//   await login(page, user);
-//   await page.getByRole('link', { name: 'Admin' }).click();
-//   expect(page.url()).toContain("/admin-dashboard");
-//   await expect(page.getByTestId("franchise-stores")).toBeVisible();
-//   const storeCount = await page
-//     .getByTestId("franchise-stores")
-//     .locator("tr")
-//     .count();
-//   expect(storeCount).toEqual(3);
-// });
+test("admin dashboard opens", async ({ page }) => {
+  const user = validUsers["admin"];
+  await login(page, user);
+  await page.getByRole("link", { name: "Admin" }).click();
+  expect(page.url()).toContain("/admin-dashboard");
+  await page.locator("tbody.divide-y").first().waitFor();
+  const franchiseCount = await page.locator("tbody.divide-y").count();
+  expect(franchiseCount).toEqual(3);
+  const totalStores = await page.locator("tr.bg-neutral-100").count();
+  expect(totalStores).toEqual(4);
+});
 
 test.describe("new test block", () => {
   test("new test template", async ({ page }) => {});
